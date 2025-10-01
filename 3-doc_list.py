4e11d8e5115ddc2aa8bc7ebb931368e1ecb10037
giangdx <giangdx@meeyland.com>
2025-10-01 15:21:44 +0700

    update

--- 3-doc_list.py
+++ 3-doc_list.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d  %H-%M-%S",
+    datefmt="%Y-%m-%d  %H:%M:%S",
 )
 
 
--- 4-divide.py
+++ 4-divide.py
@@ -11,3 +11,20 @@ Gọi thử divide(10,0) và xem log ra sao.
 
 👉 Kỳ vọng: log sẽ ghi cả traceback (giúp debug nhanh).
 """
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d  %H:%M:%S",
+    )
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        logging.error("Không thể chia cho 0", exc_info=True)
+        return None
+    
+print(divide(10,0))
